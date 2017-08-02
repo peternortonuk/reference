1769ed7f358c9055cd27462f231ba9b4d73ef2fd
pnorton <peter.norton@gazprom-mt.com>
2017-08-02 21:30:31 +0100

    wip

--- python/class.py
+++ python/class.py
@@ -33,8 +33,39 @@ class Holding(object):
     def sell(self, x):
         self.shares -= x
 
+# ==============================================================================
+# Inheritance
+
+class Parent(object):
+    def __init__(self, value):
+        self.value = value
+
+    def spam(self):
+        print('Parent spam', self.value)
+
+    def grok(self):
+        print('Parent grok')
+        self.spam()
+
+class Child1(Parent):
+    def yow(self):
+        print('Child1 yow')
+
+class Child2(Parent):
+    def spam(self):
+        print('Child2 spam', self.value)
+
+class Child3(Parent):
+    def __init__(self, i):
+        super(Child3, self).__init__(i)
+        self.value = i
+
+    def spam(self):
+        print('Child3 spam', self.value)
+        super(Child3, self).spam()
 
 
+# ==============================================================================
 if __name__ == "__main__":
     # need to instantiate the object first
     print A().f(3)
@@ -43,6 +74,7 @@ if __name__ == "__main__":
     print B.f(4)
     print C.f(5)
 
+    print('===== Instantiating and attributes =====')
     # instantiate the object
     h = Holding('AA', '2007-06-11', 100, 32.2)
 
@@ -56,6 +88,23 @@ if __name__ == "__main__":
     setattr(h, 'name', 'blahh')
     print(getattr(h, 'name'))
 
+    print('===== Inheritance =====')
+    p = Parent(25)
+    p.spam()
+    p.grok()
+
+    c1 = Child1(35)
+    print(c1.value)
+    c1.yow()
+    c1.spam()
+
+    c2 = Child2(45)
+    print(c2.value)
+    c2.spam()
+
+    c3 = Child3(55)
+    print(c3.value)
+    c3.spam()
 
 
     pass
