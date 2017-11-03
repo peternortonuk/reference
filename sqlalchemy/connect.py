1ce9f9c77c188a88c939d45ffd98b4d457b2f9fe
pnorton <peter.norton@gazprom-mt.com>
2017-11-03 16:32:49 +0000

    indents

--- sqlalchemy/connect.py
+++ sqlalchemy/connect.py
@@ -22,17 +22,18 @@ from sqlalchemy.orm import sessionmaker
 # instead, its used behind the scenes by the ORM
 Base = declarative_base()
 
+
 class User(Base):
-     __tablename__ = 'users'
+    __tablename__ = 'users'
 
-     id = Column(Integer, primary_key=True)
-     name = Column(String)
-     fullname = Column(String)
-     password = Column(String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
 
-     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-                             self.name, self.fullname, self.password)
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % \
+            (self.name, self.fullname, self.password)
 
 # create engine
 engine = create_engine('sqlite:///:memory:', echo=True)
