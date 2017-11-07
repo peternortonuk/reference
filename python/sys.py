95bde52d352b866d55cbac4bd5eb744ad28727de
Peter Norton <peter.norton.uk@gmail.com>
2017-11-07 13:24:17 +0000

    argparse playing

--- python/sys.py
+++ python/sys.py
@@ -1,1 +1,18 @@
-# args
+import sys
+
+
+# =====================================================
+# command line arguments
+
+print "The script is: ", sys.argv[0]
+print "The arguments are: "
+print sys.argv[1:]
+
+
+'''
+try this at command prompt
+python C:\dev\code\reference\python\sys.py foo bar
+
+'''
+
+# =====================================================
