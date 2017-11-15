9924d13cd5eb37894806dd408fbbd6c4cbe0eb9f
pnorton <peter.norton@gazprom-mt.com>
2017-11-15 12:13:57 +0000

    first parameterised test done

--- test/test_simple_function.py
+++ test/test_simple_function.py
@@ -1,17 +1,21 @@
 
-import mock
 import pytest
 from simple_function import add_two_numbers, mult_two_numbers, calc_two_numbers
 
+'''
+run from command line:
+> python -m pytest
+'''
 
-@pytest.mark.parametrize('expected_results', [(['add', 2, 3], 5), (['mult', 4, 5], 20)], ids=['add_scenario', 'mult_scenario'])
-@pytest.mark.parametrize('run_func', [add_two_numbers, mult_two_numbers], ids=['add', 'mult'])
-@mock.patch('simple_function.calc_two_numbers')
-def test_add_and_mult(mock_calc, run_func, expected_results):
+@pytest.mark.parametrize('expected_results',
+                         [(add_two_numbers, 2, 3, 5),
+                          (mult_two_numbers, 4, 5, 20)],
+                         ids=['add_scenario', 'mult_scenario'])
+def test_add_and_mult(expected_results):
 
-    func, arg1, arg2 = expected_results
+    func, arg1, arg2, expected_result = expected_results
 
-    assert calc_two_numbers(run_func, arg1, arg2) == expected_results
+    assert calc_two_numbers(func, arg1, arg2) == expected_result
 
 
 
