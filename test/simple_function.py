e991235845601359d6017c287df465124781533a
pnorton <peter.norton@gazprom-mt.com>
2017-11-15 10:07:41 +0000

    my first test

---
+++ test/simple_function.py
@@ -0,0 +1,13 @@
+
+
+
+def add_two_numbers(a, b):
+    return a + b
+
+def mult_two_numbers(a, b):
+    return a * b
+
+def calc_two_numbers(func, a, b):
+    return func(a, b)
+
+
---
+++ test/test_simple_function.py
@@ -0,0 +1,17 @@
+
+import mock
+import pytest
+from simple_function import add_two_numbers, mult_two_numbers, calc_two_numbers
+
+
+@pytest.mark.parametrize('expected_results', [(['add', 2, 3], 5), (['mult', 4, 5], 20)], ids=['add_scenario', 'mult_scenario'])
+@pytest.mark.parametrize('run_func', [add_two_numbers, mult_two_numbers], ids=['add', 'mult'])
+@mock.patch('simple_function.calc_two_numbers')
+def test_add_and_mult(expected_results, run_func):
+
+    func, arg1, arg2 = expected_results
+
+    assert calc_two_numbers(run_func, arg1, arg2) == expected_results
+
+
+
