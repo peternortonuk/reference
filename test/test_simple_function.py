77bf891d14613303deb466e6c1bd5fd0381246c6
pnorton <peter.norton@gazprom-mt.com>
2017-11-15 10:49:18 +0000

    wip

--- test/test_simple_function.py
+++ test/test_simple_function.py
@@ -7,7 +7,7 @@ from simple_function import add_two_numbers, mult_two_numbers, calc_two_numbers
 @pytest.mark.parametrize('expected_results', [(['add', 2, 3], 5), (['mult', 4, 5], 20)], ids=['add_scenario', 'mult_scenario'])
 @pytest.mark.parametrize('run_func', [add_two_numbers, mult_two_numbers], ids=['add', 'mult'])
 @mock.patch('simple_function.calc_two_numbers')
-def test_add_and_mult(expected_results, run_func):
+def test_add_and_mult(mock_calc, run_func, expected_results):
 
     func, arg1, arg2 = expected_results
 
