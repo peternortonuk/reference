7ef99ee26aac4363d3fa2c541c5d0f7fb8c33c9b
Peter Norton <peter.norton.uk@gmail.com>
2018-03-18 22:23:20 +0000

    wip

--- mongodb/connect.py
+++ mongodb/connect.py
@@ -1,3 +1,51 @@
-from odo.backends import mongo
+from pymongo import MongoClient
+import datetime
+import pprint as pp
 
-mongo.
+# some test data; a pretend blog post
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+contact = {"name": "Pete",
+        "phone": "07884 123123",
+        "address": ["my house, 123 the street"],}
+
+import pdb; pdb.set_trace()
+
+# start the client
+client = MongoClient()
+
+# create a new database; called test_database
+db = client.test_database
+
+# create a collection; called test_collection
+# a collection is a group of documents, roughly equivalent to a table
+collection = db.test_collection
+
+# insert blog post as a document
+# the returned object is the result of the insert
+result = collection.insert_one(post)
+
+# the result object has a record of the automatically inserted id
+id = result.inserted_id
+
+# show the object id
+print id
+
+# print the first document returned from the collection
+doc = collection.find_one()
+print doc
+
+# now insert the contact
+result = collection.insert_one(contact)
+
+# query the collection
+print collection.find_one({'text': 'My first blog post!'})
+print collection.find_one({'phone': '07884 123123'})
+
+
+# drop the database
+import pdb; pdb.set_trace()
+client.drop_database(db)
--- mongodb/readme.md
+++ mongodb/readme.md
@@ -1,2 +1,18 @@
-install using
+# install pymongo using
 >> conda install -c anaconda pymongo
+
+# download and install mongodb here:
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/#run-mongodb-community-edition
+
+# set up the mongodb environment
+>> md \data\db
+
+# start the server
+>> run.exe
+>> "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe"
+
+# beginner tutorial
+http://api.mongodb.com/python/current/tutorial.html
+
+
+
