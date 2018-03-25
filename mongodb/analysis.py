4bb7207a96b83a6d86c5b7312f94d7ad2ce2e486
Peter Norton <peter.norton.uk@gmail.com>
2018-03-25 23:31:03 +0100

    stuff

--- mongodb/analysis.py
+++ mongodb/analysis.py
@@ -1,5 +1,7 @@
+from pymongo import MongoClient
 import json
 import os
+import pprint as pp
 
 # location of json file
 rel_path = r'data'
@@ -9,6 +11,21 @@ pathfile = os.path.join(os.path.dirname(__file__), rel_path, file)
 # load into variable
 with open(pathfile) as json_data:
     data = json.load(json_data)
+
+# start the client
+client = MongoClient()
+
+# create a new database and collection
+db = client.crime_database
+collection = db.crime_database
+
+# bulk insert json data from file
+result = collection.insert_many(data)
+
+# analysis
+query = collection.find({}, {'_id': 0, 'date': 1})
+for i in query:
+    print i
 
 import pdb; pdb.set_trace()
 pass
