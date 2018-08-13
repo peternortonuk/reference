8c525e95e6f75b30462a55850093737aca2d1889
Peter Norton <peter.norton@gazprom-mt.com>
2018-08-13 21:00:28 +0100

    first mongoengine

---
+++ mongoengine/tutorial.py
@@ -0,0 +1,25 @@
+
+from tutorial_dao import *
+
+ross = User(email='ross@example.com')
+ross.first_name = 'Ross'
+ross.last_name = 'Lawley'
+ross.save()
+
+john = User(email='john@example.com')
+john.first_name = 'Johnny'
+john.last_name = 'Big bollocks'
+john.save()
+
+post1 = TextPost(title='Fun with MongoEngine', author=john)
+post1.content = 'Took a look at MongoEngine today, looks pretty cool.'
+post1.tags = ['mongodb', 'mongoengine']
+post1.save()
+
+post2 = LinkPost(title='MongoEngine Documentation', author=ross)
+post2.link_url = 'http://docs.mongoengine.com/'
+post2.tags = ['mongoengine']
+post2.save()
+
+import pdb; pdb.set_trace()
+pass
