0103a0f17e9adefdf876275c1804495ee0c398cf
pnorton <peter.norton@gazprom-mt.com>
2019-02-26 13:21:55 +0000

    consistent pyplot usage

--- matplotlib/pyplot_scatter_3d.py
+++ matplotlib/pyplot_scatter_3d.py
@@ -6,6 +6,7 @@ Colors in Matplotlib
 https://matplotlib.org/api/pyplot_summary.html#colors-in-matplotlib
 '''
 
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -21,15 +22,12 @@ x_min, x_max = x0.min() - .5, x0.max() + .5
 y_min, y_max = x1.min() - .5, x1.max() + .5
 
 # create figure
-fig2d = plt.figure(1, figsize=(8, 6))
-ax2d = fig2d.add_subplot(111)
+plt.figure(1, figsize=(8, 6))
+plt.subplot(211)
+plt.scatter(x0, y, cmap=plt.cm.Set1, edgecolor='k')
 
-fig3d = plt.figure(1, figsize=(8, 6))
-ax3d = fig3d.add_subplot(111, projection='3d')
-
-# create scatter plot
-scatter2d = ax2d.scatter(x0, y, cmap=plt.cm.Set1, edgecolor='k')
-scatter3d = ax3d.scatter(x0, x1, y, cmap=plt.cm.Set1, edgecolor='k')
+plt.subplot(212)
+plt.scatter(x0, x1, y, cmap=plt.cm.Set1, edgecolor='k')
 
 # show it
 plt.show()
