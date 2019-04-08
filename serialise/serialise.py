6c5ef09d30b324677b5927f6c9045826a45520d5
peternortonuk <peter.norton@gazprom-mt.com>
2019-04-08 21:51:29 +0100

    serialise

--- serialise/serialise.py
+++ serialise/serialise.py
@@ -1,3 +1,7 @@
+'''
+https://realpython.com/python-json/
+'''
+
 import pandas as pd
 import json, pickle
 from pprint import pprint
@@ -18,21 +22,31 @@ def deserialise(representation):
 # ==============================================================================
 import pdb; pdb.set_trace()
 
-# this is python specific object type
+# this is a python specific object type; and won't serialise directly
 obj = pd.Series([10, 11, 12], index=['2018-01-01', '2018-02-01', '2018-03-01'])
 
-# serialise it and convert to json
+# =====================================
+# serialise then convert to json
 rr = serialise(obj)
 pprint(rr)
-jj = json.dumps(rr)
+jj1 = json.dumps(rr)
+
+# or encode directly by passing the function
+jj2 = json.dumps(obj=obj, default=serialise)
+
+# pickle can serialise python objects directly
 pk1 = pickle.dumps(rr)
-pk2 = pickle.dumps(obj)  # pickle can serialise python objects directly
-pprint(jj)
+pk2 = pickle.dumps(obj)
+pprint(jj2)
 print()
 
-# take the json and deserialise
-rr = json.loads(jj)
+# =====================================
+# take the json and deserialise it
+rr = json.loads(jj1)
 pprint(rr)
-obj = deserialise(rr)
-print(obj)
+obj1 = deserialise(rr)
+
+# or decode directly by passing the function
+obj2 = json.loads(jj1, object_hook=deserialise)
+print(obj2)
 
