004c0063968db65d24642a64986d2e0a0a3fb477
pnorton <peter.norton@gazprom-mt.com>
2019-07-26 09:40:18 +0100

    list stuff

--- python/list.py
+++ python/list.py
@@ -6,3 +6,22 @@ sorted(lst)
 # sorts in place
 lst.sort()
 print(lst)
+
+a = ['aaa', 'b']
+b = ['xxxx', 'y', 'z']
+
+# this is the same as extend
+x = a + b
+print(x)
+
+# modify list in place
+a.append(b)  # add b as a single element ie nest the list
+a.extend(b)   # add the contents of b as many elements
+print(a)
+
+# apply '+' operator using sum
+lists = [['aaa', 'b'], ['xxxx', 'y', 'z']]
+x = sum(lists, [])
+print(x)
+
+pass
