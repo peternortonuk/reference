d00c1c1344f52979d1e960fe0534eec91441520b
pnorton <peter.norton@gazprom-mt.com>
2020-03-02 11:25:16 +0000

    context manager with contextlib

--- python/context_manager.py
+++ python/context_manager.py
@@ -21,3 +21,39 @@ class ContextManager():
 
 with ContextManager() as manager:
     print(manager.print_message)
+
+
+
+'''
+https://docs.python.org/3/library/contextlib.html
+
+The function being decorated must return a generator-iterator when called. 
+This iterator must yield exactly one value, which will be bound to the targets 
+in the with statement’s as clause, if any.
+'''
+
+from contextlib import contextmanager
+
+
+def acquire_resource():
+    return 1
+
+
+def release_resource(resource):
+    pass
+
+
+@contextmanager
+def managed_resource(*args, **kwds):
+    # Code to acquire resource, e.g.:
+    resource = acquire_resource(*args, **kwds)
+    try:
+        yield resource
+    finally:
+        # Code to release resource, e.g.:
+        release_resource(resource)
+
+
+with managed_resource(timeout=3600) as resource:
+    pass
+
