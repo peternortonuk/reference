94b14341274ac7dea5a1aa4b932c6fbf7aec5fa1
Peter Norton <peter.norton@gazprom-mt.com>
2020-06-27 17:32:46 +0100

    dataclass update

--- python/dataclass.py
+++ python/dataclass.py
@@ -1,9 +1,12 @@
 '''
 Raymond Hettinger - Dataclasses: The code generator to end all code generators - PyCon 2018
 https://www.youtube.com/watch?v=T-TwcmT6Rcw
+
+https://docs.python.org/3/library/dataclasses.html
+https://www.python.org/dev/peps/pep-0557/
 '''
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import NamedTuple
 
 @dataclass
@@ -11,7 +14,7 @@ class Color1:
     # dataclasses are mutable
     hue: int
     saturation: float
-    lightness: float = 0.5
+    lightness: float = field(default=0.5)  # another way to express default
 
 
 class Color2(NamedTuple):
