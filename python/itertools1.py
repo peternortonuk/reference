770bf8008609b79b1ae103ecce7b879eb30f829a
Peter Norton <peter.norton@gazprom-mt.com>
2020-09-15 21:48:33 +0100

    generators

--- python/itertools1.py
+++ python/itertools1.py
@@ -3,24 +3,81 @@ https://pythontips.com/2013/09/29/the-python-yield-keyword-explained/
 
 '''
 
+# ===================================================================
 '''
-# example 1
-mygenerator = (x*x for x in range(3))
-for i in mygenerator:
+https://www.python.org/dev/peps/pep-0289/
+generator expressions are a high performance, memory efficient generalization of list comprehensions
+'''
+print('\n=========================================================\n'
+      'example 1'
+      '\n========\n')
+
+my_generator = (x * x for x in range(3))
+for i in my_generator:
     print(i)
 
 
-# example 2
-def createGenerator():
-    mylist = range(3)
-    for i in mylist:
+# ===================================================================
+'''
+https://www.python.org/dev/peps/pep-0255/
+A function that contains a yield statement is called a generator function.
+Generator functions are a special kind of function that return a lazy iterator. 
+
+Each time the next() method of a generator-iterator is invoked, the code in 
+the body of the generator-function is executed until a yield statement is encountered.
+
+If a yield statement is encountered, the state of the function is frozen, and the 
+value of expression_list is returned to .next()'s caller. By "frozen" we mean that 
+all local state is retained, including the current bindings of local variables, 
+the instruction pointer, and the internal evaluation stack: enough information is saved 
+so that the next time .next() is invoked, the function can proceed exactly 
+as if the yield statement were just another external call.
+'''
+print('\n=========================================================\n'
+      'example 2'
+      '\n========\n')
+
+
+def create_generator(iterable):
+    for i in iterable:
         yield i*i
-mygenerator = createGenerator() # create a generator object
-for i in mygenerator:
-    print(i)
-'''
 
-# example 3
+
+my_generator = create_generator(range(3))
+print(next(my_generator))
+print(next(my_generator))
+print(next(my_generator))
+
+
+# ===================================================================
+'''
+https://realpython.com/lessons/understanding-generators/
+https://realpython.com/introduction-to-python-generators/
+'''
+print('\n=========================================================\n'
+      'example 3'
+      '\n========\n')
+
+
+def infinite_sequence():
+    num = 0
+    while True:
+        yield num
+        num += 1
+        yield print("this is the second yield statement")
+
+
+# ===================================================================
+'''
+When a return statement is encountered, control proceeds as in any function return, 
+executing the appropriate finally clauses (if any exist). Then a StopIteration exception 
+is raised, signalling that the iterator is exhausted. A StopIteration exception is 
+also raised if control flows off the end of the generator without an explicit return.
+'''
+print('\n=========================================================\n'
+      'example 4'
+      '\n========\n')
+
 bookmap = {
     'PROP': {
         'portfolio': 'PROP_OPTIONS_GAS_UK',
@@ -34,6 +91,7 @@ bookmap = {
     },
 }
 
+
 def all_curves():
     for book, book_config in bookmap.items():
         for curve in book_config['chosencurves']:
@@ -42,11 +100,12 @@ def all_curves():
                 yield curve
 
 iter_curves = all_curves()
-print(iter_curves.next())
-print(iter_curves.next())
-print(iter_curves.next())
-print(iter_curves.next())
-print(iter_curves.next())
+print(next(iter_curves))
+print(next(iter_curves))
+print(next(iter_curves))
+print(next(iter_curves))
+print(next(iter_curves))
+import pdb; pdb.set_trace()
 
 
 
@@ -55,8 +114,8 @@ def cumsum(lst):
     import pdb; pdb.set_trace()
     iter_ = iter(lst)
     while True:
-        result += iter_.next()
-    yield result
+        result += next(iter_)
+        yield result
 
 print(cumsum([1,2,3]))
 
@@ -75,10 +134,10 @@ for name in names:
 
 # under the hood its doing this...
 it = names.__iter__()
-print(it.next())
-print(it.next())
-print(it.next())
-print(it.next())
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
 
 
 # this is a generator function
@@ -88,6 +147,7 @@ def countdown(n):
         yield n
         n -= 1
     print('done')
+
 
 # this doesnt do anything except assign the generator function
 y = countdown(5)
@@ -107,18 +167,20 @@ print(squares)
 squares = (x**2 for x in nums)
 print(squares)
 
+
 # but they can't be re-used
 # one way around this is...
-
 class Countdown(object):
     def __init__(self, n):
         self.n = n
+
     def __iter__(self):
         print('counting down from ', self.n)
         while self.n > 0:
             yield self.n
             self.n -= 1
         print('done')
+
 
 c = Countdown(5)
 for x in c:
